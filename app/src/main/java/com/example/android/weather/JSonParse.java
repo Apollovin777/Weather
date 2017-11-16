36d8202affba011004b70668ae078df436ba801e
UBanit <apollovin777@gmail.com>
2017-11-16 12:36:23 +0200

    1

---
+++ app/src/main/java/com/example/android/weather/JSonParse.java
@@ -0,0 +1,55 @@
+package com.example.android.weather;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Created by ubanit on 11/16/2017.
+ */
+
+public class JSONParse {
+    private String jsonString;
+    private JSONObject jsonObject;
+
+    public JSONParse(String JSONString) throws JSONException{
+        this.jsonString = JSONString;
+        jsonObject = new JSONObject(jsonString);
+    }
+
+    public JSONParse(JSONObject jsonObject) {
+        this.jsonObject = jsonObject;
+    }
+
+    public double getTemp() throws JSONException{
+        double result = jsonObject.getJSONObject("main").getDouble("temp")-273;
+        return round(result,1);
+    }
+
+    public int getPressure() throws JSONException{
+        return jsonObject.getJSONObject("main").getInt("pressure");
+    }
+
+    public int getHumidity() throws JSONException{
+        return jsonObject.getJSONObject("main").getInt("humidity");
+    }
+
+    public double getTempMin() throws JSONException{
+        double result = jsonObject.getJSONObject("main").getInt("temp_min")-273;
+        return round(result,0);
+    }
+
+    public double getTempMax() throws JSONException{
+        double result = jsonObject.getJSONObject("main").getInt("temp_max")-273;
+        return round(result,0);
+    }
+
+    public static double round(double value, int places) {
+        if (places < 0) throw new IllegalArgumentException();
+        long factor = (long) Math.pow(10, places);
+        value = value * factor;
+        long tmp = Math.round(value);
+        return (double) tmp / factor;
+    }
+
+
+}
--- app/src/main/java/com/example/android/weather/MainActivity.java
+++ app/src/main/java/com/example/android/weather/MainActivity.java
@@ -2,21 +2,33 @@ package com.example.android.weather;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkError;
+import com.android.volley.NoConnectionError;
+import com.android.volley.ParseError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
+import com.android.volley.ServerError;
+import com.android.volley.TimeoutError;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
     private EditText mEditText;
     private TextView mTextView;
-
+    private JSONParse resultOfRequest;
+    private VolleyError requestError;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,28 +40,59 @@ public class MainActivity extends AppCompatActivity {
 
     public void onPressSearch(View view) {
         RequestQueue queue = Volley.newRequestQueue(this);
-        String url ="http://api.openweathermap.org/data/2.5/weather?q=";
+        String url = "http://api.openweathermap .org/data/2.5/weather?q=";
         String appid = "f0fd052a5dd68c962d6cc9aa80735ed4";
-
         String userInput = mEditText.getText().toString();
-
-        if(userInput!=null && userInput.length()>2) {
+        if (userInput != null && userInput.length() > 2) {
             String urlForRequest = url + userInput + "&appid=" + appid;
-           StringRequest stringRequest = new StringRequest(Request.Method.GET, urlForRequest,
-                    new Response.Listener<String>() {
+            JsonObjectRequest jsObjRequest = new JsonObjectRequest
+                    (Request.Method.GET, urlForRequest, null, new Response.Listener<JSONObject>() {
                         @Override
-                        public void onResponse(String response) {
-                            // Display the first 500 characters of the response string.
-                            mTextView.setText("Response is: " + response.substring(0, 200));
+                        public void onResponse(JSONObject response) {
+                            resultOfRequest = new JSONParse(response);
+                            UpdateField();
                         }
                     }, new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(VolleyError error) {
-                    mTextView.setText("That didn't work!");
-                }
-            });
-// Add the request to the RequestQueue.
-            queue.add(stringRequest);
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            requestError = error;
+                            OnErrorUpdate();
+                        }
+                    });
+            queue.add(jsObjRequest);
         }
+
+
+    }
+    private void OnErrorUpdate(){
+            if (requestError instanceof TimeoutError || requestError instanceof NoConnectionError) {
+                Log.i("Error request", "TimeoutError");
+            } else if (requestError instanceof AuthFailureError) {
+                Log.i("Error request", "AuthFailureError");
+            } else if (requestError instanceof ServerError) {
+                Log.i("Error request", "ServerError");
+            } else if (requestError instanceof NetworkError) {
+                Log.i("Error request", "NetworkError");
+            } else if (requestError instanceof ParseError) {
+                Log.i("Error request", "ParseError");
+            }
+
+    }
+
+    private void UpdateField(){
+
+            StringBuilder builder = new StringBuilder();
+            try {
+                builder.append("Temp: " + resultOfRequest.getTemp() + "\n");
+                builder.append("Humidity: " + resultOfRequest.getHumidity() + "\n");
+                builder.append("Pressure: " + resultOfRequest.getPressure() + "\n");
+                builder.append("Temp_min: " + resultOfRequest.getTempMin() + "\n");
+                builder.append("Temp_max: " + resultOfRequest.getTempMax() + "\n");
+                mTextView.setText(builder.toString());
+            } catch (JSONException e) {
+                mTextView.setText(e.getMessage());
+            }
+
+
     }
 }
